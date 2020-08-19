e6a36d24ab5f397fc0a10979b20008b399a380c8
TapAlexxx <lordpictures123@gmail.com>
2020-08-19 17:03:40 +0300

    MonsterSpawner

---
+++ EnemySpotted/Assets/Skriptes/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+public class Monster : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime;
+
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("Idle");
+
+        Destroy(gameObject, _lifeTime);
+    }
+}
---
+++ EnemySpotted/Assets/Skriptes/MonsterCreator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCreator : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPositions;
+    [SerializeField] private GameObject[] _monsters;
+    [SerializeField] private float _timeBetweenSpawning;
+    
+
+    private void Start()
+    {
+        StartCoroutine(SpawnNewEnemies(_timeBetweenSpawning));
+    }
+
+    private IEnumerator SpawnNewEnemies(float timeBetweenSpawning)
+    {
+        var waitForSeconds = new WaitForSeconds(timeBetweenSpawning);
+        while (true)
+        {
+            int randomMonsterNumber = Random.Range(0, _monsters.Length);
+            int randomSpawnPosition = Random.Range(0, _spawnPositions.Length);
+
+            Instantiate(_monsters[randomMonsterNumber], _spawnPositions[randomSpawnPosition].position, Quaternion.identity);
+            yield return waitForSeconds;
+        }
+    }
+}
